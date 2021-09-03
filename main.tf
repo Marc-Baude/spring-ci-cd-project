f0264114049bfa128389deeee0b354c7162491bd
Marc Baude <marc.baude@pexon-consulting.de>
2021-09-03 11:55:24 +0200

    Add terraform script

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  backend "gcs" {
+    bucket = "pexon-training-tfstate"
+    prefix = "marc-training/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "pexon-training"
+  region      = "europe-west4"
+}
+resource "google_sql_database_instance" "master" {
+  name             = "marcinstance"
+  database_version = "MYSQL_5_7"
+  region           = "europe-west4"
+  settings {
+    tier = "db-f1-micro"
+
+  }
+}
+
+resource "google_sql_database" "database" {
+  name      = "database_name"
+  instance  = google_sql_database_instance.master.name
+  charset   = "utf8"
+  collation = "utf8_general_ci"
+}
+
+resource "google_sql_user" "users" {
+  name     = "root"
+  instance = google_sql_database_instance.master.name
+  host     = "%"
+  password = "root"
+}
+
+output "db_ip" {
+  value = google_sql_database_instance.master.ip_address
+}
