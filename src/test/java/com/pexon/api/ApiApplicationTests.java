0109186df8cdd0270b50cd2a8324e1f40e863fc8
Marc Baude <marc.baude@pexon-consulting.de>
2021-09-03 11:07:30 +0200

    Add test to cloudbuild.yaml

--- src/test/java/com/pexon/api/ApiApplicationTests.java
+++ src/test/java/com/pexon/api/ApiApplicationTests.java
@@ -2,12 +2,24 @@ package com.pexon.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class ApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
+		System.out.println("Test 1");
+	}
+
+	@Test
+	void testTest()
+	{
+		System.out.println("Test 2");
 	}
 
 }
