adeeee363a410a10f4605e3e5bcea9a9aa4c6d6b
Marc Baude <marc.baude@pexon-consulting.de>
2021-09-10 16:28:44 +0200

    Readme changes + build args

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-springclone
+# Spring API with CI/CD - Example Project
+
+### Author : Marc Baude
+
+#### Description:
+_This is an example project that maybe can help you with your own._
--- src/main/java/com/pexon/api/test/TestController.java
+++ src/main/java/com/pexon/api/test/TestController.java
@@ -1,7 +1,9 @@
 package com.pexon.api.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 class TestController {
     //This is a test request if the db connection cannot be established
     @GetMapping(path="api/v1/test")
